b1a86fc1e2f0bc74961ad152a632389be301a616
reddyin12 <gindra.kp86@gmail.com>
2019-01-11 11:30:50 -0800

    First change

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //pause to display helloworld
+            Console.WriteLine("Press any Key to continue");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
